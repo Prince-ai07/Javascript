fe791c227436c728a9aade00bf5d88eb66c1a9c6
Prince-ai07 <jeremiahprince342@gmail.com>
2026-02-26 20:57:54 +0300

    added lesson2

---
+++ test.py
@@ -0,0 +1,19 @@
+# What is the difference between a dictionary in python, an object in Javascript and a json data when dealing with APIs
+
+age = 20
+if age < 18:
+    print("Program Guide: Not suitable for children under the age of 18")
+else:
+    print("Sit back, relax and enjoy...")
+
+
+age = 41
+
+if age < 18: 
+    print("You are too young to watch this episode.")
+elif age >= 15 and age <=18:
+    print("You can watch this episode with parental guidance...")
+elif age > 18 and age < 40:
+    print("Have a seat and enjoy the episode")
+else:
+    print("This episode is not suitable for you.")

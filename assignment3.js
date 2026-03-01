eae43dd0d9dce0a71e6386a1bf605a125736acca
Prince-ai07 <jeremiahprince342@gmail.com>
2026-03-01 17:42:28 +0300

    added assignment 3

--- assignment3.js
+++ assignment3.js
@@ -60,3 +60,38 @@ while(r >= 1) {
     console.log(r);
     r--;
 }
+
+// Come up with a program that will store some Gross Salary in Variable, Then using If Else...Else if statements Determine the monthly contribution someone will pay.
+console.log("=============================")
+let gross_income = 120000
+
+if(gross_income < 5999) {
+    console.log(150)
+}
+else if(gross_income >= 6000 && gross_income <= 7999) {
+    console.log(300)
+}
+else if(gross_income >= 8000 && gross_income <= 11999) {
+    console.log(400)
+}
+else if(gross_income >= 12000 && gross_income <= 14999) {
+    console.log(500)
+}
+else if(gross_income >= 15000 && gross_income <= 19999){
+    console.log(600)
+}
+else if(gross_income >= 20000 && gross_income <= 24999){
+    console.log(750)
+}
+else if(gross_income >= 25000 && gross_income <= 29999){
+    console.log(850)
+}
+else if(gross_income >= 30000 && gross_income <= 49999){
+    console.log(1000)
+}
+else if(gross_income >= 50000 && gross_income <= 99999){
+    console.log(1500)
+}
+else{
+    console.log(2000)
+}
